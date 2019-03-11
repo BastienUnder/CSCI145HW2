b4c852bce1d3edeb94e72c4735789db92ce000e4
BastienUnder <46801793+BastienUnder@users.noreply.github.com>
2019-03-11 15:55:13 -0700

    Add files via upload

--- MatrixReader.java
+++ MatrixReader.java
@@ -8,6 +8,7 @@ public class MatrixReader {
 
     public SparseMatrix read(String file){
 
+
         //Access and read our sparse matrix file!!
         try{
             File f = new File(file);
@@ -19,6 +20,79 @@ public class MatrixReader {
             int columns = input.nextInt();
             input.nextLine();
 
+
+            // create new SparseMatrix called "spar"
+            SparseMatrix spar = new SparseMatrix(rows, columns);
+
+            System.out.println("MAtricReder Wroked!");
+
+            //read the matrix provided and save the contents of our sparse matrix as head nodes and value nodes
+            int rowNum = 1;
+            //iterate through each row of the matrix, downwards
+            while(input.hasNextLine()){
+
+                String[] currentLine = input.nextLine().split(" ");
+
+                //iterate through each column to the right
+                for(int i = 0; i < currentLine.length; i++){
+
+                    String[] individual = currentLine[i].split(",");
+
+                    int columnNum = Integer.parseInt(individual[0]);
+                    int value = Integer.parseInt(individual[1]);
+
+                    //call insert on SparseMatrix
+                    spar.insert(rowNum, columnNum, value);
+
+                    // A TEST FOR CHECKING CURRENT LINE VALUES:
+                    //System.out.print(currentLine[i] + " " );
+                }
+                rowNum++;
+
+                /*if(!currentLine.equals(null)){
+                    String[] rowFirstValueNode = currentLine[0].split(",");
+                    System.out.println(rowFirstValueNode[0]);
+                }*/
+                //System.out.println();
+            }
+            //return our sparse matrix all nice and pretty looking!
+            return spar;
+        }
+
+        //Error out if our file isn't find
+        catch(NoSuchElementException | IOException e){
+            System.out.println("File not found.");
+            e.printStackTrace();
+        }
+
+        //return
+        System.out.println("Uh Oh, looks like you crashed windows, need some help?");
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
+    }
+
+       /* //Access and read our sparse matrix file!!
+        try{
+            File f = new File(file);
+            Scanner input = new Scanner(f);
+
+            //Set the total rows and columns and save them in our sparse matrix
+            int rows = input.nextInt();
+            input.nextLine();
+            int columns = input.nextInt();
+            input.nextLine();
+
+            // create new SparseMatrix called "spar"
             SparseMatrix spar = new SparseMatrix(rows, columns);
 
 
@@ -75,5 +149,7 @@ public class MatrixReader {
         //return
         System.out.println("Uh Oh, looks like you crashed windows, need some help?");
         return null;
-    }
-}
+    }*/
+
+
+
--- SparseMatrix.java
+++ SparseMatrix.java
@@ -7,7 +7,7 @@ public class SparseMatrix {
     //Sparse Matrix logic
     public SparseMatrix(int rows, int columns){
         System.out.println("WAHOO");
-        
+
         this.totalRows = rows;
         this.totalColumns = columns;
 
@@ -33,15 +33,17 @@ public class SparseMatrix {
     public void insert(int row, int column, int value){
         ValueNode nodeToInsert = new ValueNode();
         nodeToInsert.setValue(value);
+        nodeToInsert.setColumn(column);
+        nodeToInsert.setRow(row);
 
         //Insert the new valueNode into the right row
         HeadNode rowHead = getRow(row);
-        nodeToInsert.setRow(row);
+        //nodeToInsert.setRow(row);
         rowHead.insert(nodeToInsert); //need to implement matrixRow and matrixColumn insert()
 
         //insert the new valueNode into the right column
         HeadNode columnHead = getColumn(column);
-        nodeToInsert.setColumn(column);
+        //nodeToInsert.setColumn(column);
         columnHead.insert(nodeToInsert);
     }
 
@@ -62,23 +64,113 @@ public class SparseMatrix {
     }
 
     public int getValue(int row, int column){
-        HeadNode curColumn = firstColumn;
-        HeadNode curRow = firstRow;
-        for(int i = 0; i < column; i++){
+        HeadNode curColumn = (HeadNode)firstColumn.getNextRow();//getNext
+        HeadNode curRow = (HeadNode)firstRow.getNextColumn();//getNext
+        for(int i = 1; i < column; i++){
             curColumn = curColumn.getNext();
         }
-        for(int i = 0; i < row; i++){
+        for(int i = 1; i < row; i++){
             curRow = curRow.getNext();
         }
+
+//THe top!!!!
+        //if the location being looked for doesn't exist
+        if(curRow.getNextColumn() == null){
+            return 0;
+        }
+
+
+        //if we want to insert into a further column than the first one is in
+        else if(getFirst().getColumn() < value.getColumn()){
+            ValueNode cur = getFirst();
+
+            //if there's only one valueNode in that row set the new ValueNode for right after
+            if(cur.getNextColumn() == null){
+                cur.setNextColumn(value);
+            }
+            else {
+                rowLoop: for (int i = 1; i < value.getColumn() + 1; i++) {
+                    if (cur.getNextColumn() != null) {
+                        if (cur.getNextColumn().getColumn() > value.getColumn() && value.getColumn() == i) {
+                            value.setNextColumn(cur.getNextColumn());
+                            cur.setNextColumn(value);
+                            break rowLoop;
+                        }
+                        else if(cur.getNextColumn().getColumn() > value.getColumn() && i < value.getColumn()){
+                            //run the for loop again to be in the right column
+                        }
+
+                        else {
+                            cur = cur.getNextColumn();
+                        }
+                    }
+
+                    else {
+                        cur.setNextColumn(value);
+                        break rowLoop;
+                    }
+                }
+            }
+
+            /*while(cur.getNextColumn() != null){
+
+                if(cur.getNextColumn().getColumn() > value.getColumn()){
+                    cur.setNextColumn(value);
+                    break;
+                }
+                else{
+                    cur = cur.getNextColumn();
+                }
+            }*/
+        }
+
+        //if we want to insert into a column before the first valueNode in that row
+        else{
+            value.setNextColumn(getFirst());
+            setNextColumn(value);
+        }
+
+    }
+
+    //The Bottom!!!
+
+
+
+
 
 
         return 0;
     }
 
     public void print(){
+        //print needs to print out the zeros before each of the matrix row's
+        //first value(getFirst) as well as the zeros in between, print out
+        //zeros for the difference between the far valueNode in the row
+        //and the close(first, smaller) valeNode
+        HeadNode curRow = (HeadNode)firstRow.getNextRow();
+        ValueNode curCol = ((MatrixRow)firstRow.getNextRow()).getNextColumn();//.getNextColumn();//or curRow?? not firstRow
+        for(int i = 1; i < totalRows + 1; i++){
+            //HeadNode actualFirst = (HeadNode)firstRow.getNextRow();//AHHHHHHHH
+            //ValueNode curCol = ((MatrixRow)firstRow.getNextRow()).getNextColumn();//.getNextColumn();//or curRow?? not firstRow
+            for(int j = 1; j < totalColumns + 1; j++){
+                if(curCol != null && curCol.getColumn() == j){
+                    System.out.print(curCol.getValue() + " ");
+                    curCol = curCol.getNextColumn();
+                }
+                else{
+                    System.out.print("0" + " ");
+                }
+            }
+            System.out.println();
+            curRow = (MatrixRow)curRow.getNextRow();
 
+            if(curRow != null) {
+                curCol = (ValueNode) curRow.getNextColumn();
+            }
+        }
     }
     public SparseMatrix transpose(){
+        //
         return null;
     }
 
